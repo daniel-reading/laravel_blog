7ea24fe8fcfdafb45de9709d900f3a088c95dd06
Daniel Reading <readingaa0001@gnail.com>
2024-05-17 09:58:05 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [RoleController::class, 'index'])->name('role.index');
+
+Route::prefix('/Role')->group(function(){
+
+    Route::get('/Create', [RoleController::class, 'create'])->name('role.create');
+
+    Route::post('/Store', [RoleController::class, 'store'])->name('role.store');
+
+    Route::delete('/{role}/Delete', [RoleController::class, 'destroy'])->name('role.delete');
+
+    Route::get('/{role}/Update', [RoleController::class, 'update'])->name('role.update.page');
+
+    Route::put('/{role}/Edit', [RoleController::class, 'change'])->name('role.update.handle');
 });
-route::get('/role',[RoleController::class, 'index'])->name('role_index');
+
